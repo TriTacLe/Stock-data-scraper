0da69470440a0a02c531efc80a272600b9bef694
Tri <tritac.le@gmail.com>
2025-02-22 18:27:56 +0100

    Fixed bug. Added more values

--- backend/app.py
+++ backend/app.py
@@ -22,11 +22,38 @@ def home():
 def stock_data(ticker: str):
   try:
     stock = yf.Ticker(ticker)
-    price = stock.history(period="1d")["Close"].iloc[-1]
+    history = stock.history(period="1d")
     
+    #sanntidspris
+    price = float(history["Close"].iloc[-1])
+    open_price = float(history["Open"].iloc[-1])
+    high_price = float(history["High"].iloc[-1])
+    low_price = float(history["Low"].iloc[-1])
+    volume = float(history["Volume"].iloc[-1])
+
+    #Funamdentaldata, skal flyttes over til scraping.py for høyere cohesion
+    info = stock.info
+    pe = float(info.get("trailingPE", None))
+    marketcap = float(info.get("marketCap", None))
+    dividend_yield = float(info.get("dividendYield", None))
+
+    #Ta indikatorer
+    ma_50 = float(stock.history(period="50d")["Close"].mean())
+    ma_200 = float(stock.history(period="200d")["Close"].mean())
+
     return {
       "ticker": ticker,
-      "price": price
+      "price": price,
+      "open_price": open_price,
+      "high_price": high_price,
+      "low_price": low_price,
+      "volume": volume,
+      "marketcap": marketcap,
+      "pe_ratio": pe,
+      "dividend_yield": dividend_yield,
+      "moving_avg_50": ma_50,
+      "moving_avg_200": ma_200
+
     }
   except Exception as e:
     raise HTTPException(status_code=404, detail="Error fetching stock data")
